2ddcee573c2a36c0c995d3a85e5bee906fec62c2
TajsHjul <73933068+TajsHjul@users.noreply.github.com>
2021-01-11 07:31:11 +0100

    Endnu en test

---
+++ TestAppProject1/TestAppProject1/Filefinder.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using UserControl = System.Windows.Controls.UserControl;
+using MessageBox = System.Windows.Forms.MessageBox;
+using System.Net;
+
+namespace TestAppProject1
+{
+    /// <summary>
+    /// Interaction logic for Filefinder.xaml
+    /// </summary>
+    public partial class Filefinder : UserControl
+    {
+        public Filefinder()
+        {
+            InitializeComponent(); this.DataContext = this;
+        }
+        private void Select_click(object sender, EventArgs e)
+        {
+            string fileContent = string.Empty;
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv|Excel files (*.xlsx*)|*.*|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+
+                    }
+                }
+            }
+
+
+        }
+        private void Update_click(object sender, EventArgs e)
+        {
+            
+            
+        }
+        private void Download_click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+    }
+}
